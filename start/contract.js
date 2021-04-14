bf83942e29e4fec39ae16c67e6b82e786ccba8c3
ansharah-gs <ansharahmir@gmail.com>
2021-04-14 19:13:49 +0500

    first Commit

--- start/contract.js
+++ start/contract.js
@@ -33,6 +33,45 @@ The `piggybankContract` is compiled from:
 const forwarderOrigin = 'http://localhost:9010'
 
 const initialize = () => {
-  //You will start here 
-}
+  //Basic Actions Section
+  const onboardButton = document.getElementById('connectButton');
+
+  //Created check function to see if the MetaMask extension is installed
+  const isMetaMaskInstalled = () => {
+    //Have to check the ethereum binding on the window object to see if it's installed
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
+  //We create a new MetaMask onboarding object to use in our app
+const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
+
+//This will start the onboarding proccess
+const onClickInstall = () => {
+  onboardButton.innerText = 'Onboarding in progress';
+  onboardButton.disabled = true;
+  //On this object we have startOnboarding which will start the onboarding process for our end user
+  onboarding.startOnboarding();
+};
+
+  //check if metamask is installed
+  const MetaMaskClientCheck = () => {
+    //Now we check to see if Metmask is installed
+    if (!isMetaMaskInstalled()) {
+      //If it isn't installed we ask the user to click to install it
+      onboardButton.innerText = 'Click here to install MetaMask!';
+      //When the button is clicked we call this function
+      onboardButton.onclick = onClickInstall;
+      //The button is now disabled
+      onboardButton.disabled = false;
+    } else {
+      //If it is installed we change our button text
+      onboardButton.innerText = 'Connect';
+       //When the button is clicked we call this function to connect the users MetaMask Wallet
+      onboardButton.onclick = onClickConnect;
+      //The button is now disabled
+      onboardButton.disabled = false;
+    }
+  };
+  MetaMaskClientCheck();
+};
 window.addEventListener('DOMContentLoaded', initialize)
